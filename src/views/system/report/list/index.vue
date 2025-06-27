4ca43da36e11758d8265d3d8f2d6a09e18754602
kagg886 <iveour@163.com>
2025-06-27 17:12:13 +0800

    fix bug

--- src/views/system/report/list/index.vue
+++ src/views/system/report/list/index.vue
@@ -409,7 +409,7 @@ const {loading: createFeedbackLoading, doLoading: createFeedback} = useLoading(a
 	if (result) {
 		ElMessage.success('反馈成功')
 	}
-	handleEditCancel()
+	handleFeedbackCancel()
 })
 </script>
 
